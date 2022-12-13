32fd5370bc600e394150ef206b36ab19c5484ee8
001496270_TongZhou <44611330+Dabaiee@users.noreply.github.com>
2022-12-13 09:50:01 -0800

    join group， check status

--- src/details/detail.js
+++ src/details/detail.js
@@ -4,12 +4,12 @@ import {useParams, useLocation} from "react-router-dom";
 import NewReview from "./new-review";
 import ReviewList from "./review-list";
 import {useDispatch, useSelector} from "react-redux";
-import {findJoinedThunk, joinGroupThunk} from "../services/group-thunks";
+import {checkJoinedThunk, findJoinedThunk, joinGroupThunk} from "../services/group-thunks";
 import {findReviewsThunk} from "../services/review-thunks";
 import GameCard from "../igdb/igdb-card";
 
 function DetailComponent() {
-    const {joined} = useSelector(state => state.group)
+    const {joinedThis} = useSelector(state => state.group)
     const {currentUser} = useSelector((state) => state.users)
     const params = useParams();
     const location = useLocation();
@@ -18,12 +18,14 @@ function DetailComponent() {
     const dispatch = useDispatch();
     const joinGroupHandler = () => {
         dispatch(joinGroupThunk({uid: currentUser._id, gid:gameId.toString()}));
+        dispatch(checkJoinedThunk({uid: currentUser._id, gid:gameId.toString()}));
     }
     useEffect(() => {
-        dispatch(findJoinedThunk(currentUser._id))
+        dispatch(checkJoinedThunk({uid: currentUser._id, gid:gameId.toString()}));
     }, [])
 
-    // console.log(joined[0])
+
+    // console.log(joinedThis)
     return(
         <div className="container">
             {/*{*/}
@@ -42,16 +44,16 @@ function DetailComponent() {
                 <div className="align-self-center ">
                     <h2>{game.Title}</h2>
                     <div>Release year: {game.Year}</div>
-                    <button className={`
+                    <button className={`${joinedThis? "d-none": ""}
                     rounded-pill btn btn-primary float-end mt-2 ps-3 pe-3 fw-bold`}
                             onClick={joinGroupHandler}>
                         Join Fans Group of {game.Title}
                     </button>
-                    {/*<button className={`${!joined? "d-none": ""}*/}
-                    {/*rounded-pill btn btn-disabled float-end mt-2 ps-3 pe-3 fw-bold`}>*/}
-                    {/*    Has Joined this Group*/}
-                    {/*    <i className="bi bi-check-lg align-self-center p-0 m-0"></i>*/}
-                    {/*</button>*/}
+                    <button className={`${joinedThis? "": "d-none"}
+                    rounded-pill btn btn-disabled float-end mt-2 ps-3 pe-3 fw-bold`}>
+                        Has Joined this Group
+                        <i className="bi bi-check-lg align-self-center p-0 m-0"></i>
+                    </button>
                 </div>
                 <div></div>
             </div>

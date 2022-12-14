56626fb4b8ba2c17e8ad5ccf51003ecc1654043a
001496270_TongZhou <44611330+Dabaiee@users.noreply.github.com>
2022-12-14 09:59:41 -0800

    history card

--- src/users/history.js
+++ src/users/history.js
@@ -13,14 +13,15 @@ const HistoryComponent = ({uid}) => {
         dispatch(findUserByIdThunk(uid))
         dispatch(findHistoryByIdThunk(uid))
     }, [])
+    console.log(history)
     return (
         <>
             <h2>Recent Viewed</h2>
-            <div className="list-group">
+            <div className="list-group d-flex flex-row flex-wrap">
                 {
-                    history && history.map((history) =>
+                    history && history.map((post) =>
                         // <li>{history.Card}</li>
-                        <HistoryCard key={history.hID}/>
+                        <HistoryCard history= {post}/>
                     )
                 }
             </div>

04b981418723b3c255b11e6aaeb9663b09eca37e
001496270_TongZhou <44611330+Dabaiee@users.noreply.github.com>
2022-12-14 10:14:30 -0800

    public prof

--- src/users/public-profile.js
+++ src/users/public-profile.js
@@ -5,21 +5,32 @@ import {useDispatch, useSelector} from "react-redux";
 import {findUserByIdThunk} from "../services/users-thunks";
 import {findJoinedThunk} from "../services/group-thunks";
 import JoinedGroups from "./joined-groups";
+import ProfileCard from "./profile-card";
 
 
 
 const PublicProfile = () => {
     const {uid} = useParams()
-    // const {publicProfile} = useSelector((state) => state.users)
-    console.log(uid)
+    const {publicProfile} = useSelector((state) => state.users)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(findUserByIdThunk(uid))
+        // dispatch(findReviewsByAuthorThunk(uid))
+        // dispatch(findFollowersThunk(uid))
+        // dispatch(findFollowingThunk(uid))
+    }, [uid])
+    console.log(publicProfile)
     return(
         <>
             <img className="w-100 " src="/images/banner.jpg" alt="banner"/>
             <img src="/images/hao.jpg" alt={"avatar"}
                  className="rounded-circle w-25 float-left ms-3 position-relative top-0 translate-middle-y"/>
 
-            <div className="container">
-                {/*<h1>{publicProfile && publicProfile.username}'s profile</h1>*/}
+            <div className="container mb-5">
+                <h1>{publicProfile && publicProfile.username}'s profile</h1>
+                <ProfileCard/>
+                <br/><br/>
                 <JoinedGroups uid={uid}/>
             </div>
 
